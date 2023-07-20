9ffa5c4785a3b135cac0830ecc9014642baa36a1
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2023-07-20 16:58:09 +0530

    added support for multiple api keys

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -3,18 +3,24 @@ import 'dart:convert';
 import 'config.dart';
 
 class ApiService {
-  static final Uri baseUrl = Uri.parse('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=3df2416abe25496a80263ff0c5d7d6c3');
-  static const String apiKey = Config.apiKey;
+  static const String _baseUrl = 'https://newsapi.org/v2/top-headlines';
+  static const String _countryCode = 'us';
+  static const String _category = 'business';
 
   static Future<List<Map<String, dynamic>>> fetchNews() async {
+    String apiKey = Config.getCurrentApiKey(); // Get the current API key
+
     final response = await http.get(
-      baseUrl,
-      headers: {'Authorization': apiKey},
+      Uri.parse('$_baseUrl?country=$_countryCode&category=$_category&apiKey=$apiKey'),
     );
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
       return List<Map<String, dynamic>>.from(data['articles']);
+    } else if (response.statusCode == 429) {
+      // Quota exhausted, switch to the next API key and retry
+      apiKey = Config.switchToNextApiKey();
+      return await fetchNews(); // Retry with the next API key
     } else {
       throw Exception('Failed to fetch news');
     }
